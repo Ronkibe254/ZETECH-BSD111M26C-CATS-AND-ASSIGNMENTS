4957c5c8809185b86b29d44efd22982f57de75eb
ron kiberenge <ronkibe254@gmail.com>
2026-06-13 14:27:38 +0300

    Add Week 4 Activity: For loop printing 100 to 50 descending

---
+++ week4Assignment1.c
@@ -0,0 +1,10 @@
+// For loop to print down from 100 to 50
+#include <stdio.h>
+    int main (){
+        int i;
+
+        for(i=100;i>=50;i--){
+            printf("%d\t",i);
+        }
+        return 0;
+    }
